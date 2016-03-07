94f00a37d8da56ffb4dd904ba57d72c9b1bda12f
hy-yoneda <2377065+hy-yoneda@users.noreply.github.com>
2016-03-07 18:44:43 +0900

    add svn-cleanup command

--- .zsh/20_utils.zsh
+++ .zsh/20_utils.zsh
@@ -1,2 +1,7 @@
-# 関数
+# 検索
 find-grep () { find . -type f -print | xargs grep -n --binary-files=without-match $@ }
+
+# svn cleanup
+cleanup-svn () {
+    find $@ -type d -name ".svn" -print | xargs -i sh -cx "svn cleanup {}/.."
+}
