490500e61001532ef47fe61eee44d32fa9ebffc8
hy-yoneda <2377065+hy-yoneda@users.noreply.github.com>
2016-07-14 12:35:45 +0900

    fixed .zsh

--- .zsh/20_utils.zsh
+++ .zsh/20_utils.zsh
@@ -5,3 +5,13 @@ find-grep () { find . -type f -print | xargs grep -n --binary-files=without-matc
 cleanup-svn () {
     find $@ -type d -name ".svn" -print | xargs -i sh -cx "svn cleanup {}/.."
 }
+
+# create server command
+create_server_command () {
+    echo 'for i in $(seq '$1'); do for j in $(seq -w '$2'); do echo ${i}${j}; done; done | xargs -i '"${@:3}"''
+}
+
+# enum server
+enum_server () {
+    for i in $(seq $1); do for j in $(seq -w $2); do echo ${i}${j}; done; done
+}
--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -1,11 +1,21 @@
 ## alias
 alias vi='vim'
-alias ls='ls -AFG'
-alias ll='ls -lFG'
-alias la='ls -laAFG'
 alias df="df -h"
 alias du="du -h"
 alias screen='screen -U'
 alias h='history'
 alias less="$(which less) -r"
 alias gdir='GDIR=$(git dir) && cd ${GDIR}'
+
+case "${OSTYPE}" in
+    darwin*)
+        alias ll='ls -lFG'
+        alias la='ls -laFG'
+        alias ls='ls -AFG'
+        ;;
+    *)
+        alias ll='ls -lF --color'
+        alias la='ls -laF --color'
+        alias ls='ls -AF --color'
+        ;;
+esac
